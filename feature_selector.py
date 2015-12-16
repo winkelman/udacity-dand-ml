fc90280a9d22d208f94bea3f699d6ad344036ce8
winkelman <christopher.m.winkelman@gmail.com>
2015-12-16 03:08:50 -0800

    improve feature selection

--- feature_selector.py
+++ feature_selector.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-from sklearn.linear_model import RidgeClassifierCV
+from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import RFE
 from sklearn.feature_selection import SelectKBest
 from sklearn.cross_validation import train_test_split
@@ -34,7 +34,7 @@ def select_best(data_dict, features_list, n = 10):
 	df = df[selection]
 	## syntax below will not preserve the 'poi' feature, it was a first attempt at removing the zero rows
 	#df = df[(df.drop('poi', axis=1)) != 0].dropna(axis=0, how='all').replace(np.nan, 0)
-
+        
         
         
         ### splitting data into features and labels with test and training sets
@@ -51,7 +51,40 @@ def select_best(data_dict, features_list, n = 10):
         
         
         
+        ### keep track of total ranking of features
+        total_rank = {pred: 0 for pred in predictors}
+        
+        
+        
+        ### getting best features based upon UNIVARIATE PEARSON CORRELATION score
+        
+        cor_scores = []
+        for ftr in predictors:
+            poi_m = df['poi'].as_matrix()
+            ftr_m = df[ftr].as_matrix()
+            cor = pearsonr(poi_m,ftr_m)[0]
+            cor_scores.append([ftr, cor])
+            
+        cor_scores = sorted(cor_scores, key = lambda x: x[1], reverse = True)
+        cor_scores_dict = {feature: value for (feature, value) in cor_scores} ## for use in identifying high correlation pairs
+        cor_selection = [pair[0] for pair in cor_scores[:n]]
+        
+        ## scores for aggregation
+        cor_rank = list(enumerate(cor_selection[::-1], start = 1))
+        for e in cor_rank:
+            total_rank[e[1]] += e[0]
+        
+        worst = [pair[0] for pair in cor_scores if pair[1] < 0.25] ## most poorly correlated feature
+        print "\nFeature correlations with target POI: ", cor_scores
+        print "Best features from univariate pearson correlation analysis (low to high): ", cor_rank
+        print "Worst features with less than 0.25 linear correlation with POI: ", worst
+        
+        
+        
+        # SelectKBest scoring method is questionable, not using...
+        '''
         ### getting best features based upon univariate SelectKBest
+        
         kbest = SelectKBest(k = n).fit(features_train, labels_train)
         
         ## using dictionary
@@ -64,10 +97,11 @@ def select_best(data_dict, features_list, n = 10):
         ## syntax below is faster for larger datasets but requires another library
         #from operator import itemgetter
         #features_scores sorted(zip(predictors, kbest.scores_), key=itemgetter(1), reverse = True)
-        kbest_selection = [pair[0] for pair in features_scores[:n]] ## uncomment if 
+        kbest_selection = [pair[0] for pair in features_scores[:n]]
             
         print "\nBest features from k-best univariate analysis: ", kbest_selection
         print "Feature Scores: ", features_scores
+        '''
         
         
         
@@ -75,9 +109,9 @@ def select_best(data_dict, features_list, n = 10):
         
         ## getting a list of variable pairs
         pairs = []
-        for i in range(0, len(kbest_selection)):
-            for j in range(i + 1, len(kbest_selection)):
-                pairs.append([kbest_selection[i],kbest_selection[j]])
+        for i in range(0, len(cor_selection)):
+            for j in range(i + 1, len(cor_selection)):
+                pairs.append([cor_selection[i],cor_selection[j]])
         
         ## just for fun
         '''pairs = set()
@@ -112,22 +146,28 @@ def select_best(data_dict, features_list, n = 10):
         for e in high_corr:
             var1_name = e[0]
             var2_name = e[1]
-            var1_score = features_scores_dict[var1_name]
-            var2_score = features_scores_dict[var2_name]
+            var1_score = cor_scores_dict[var1_name]
+            var2_score = cor_scores_dict[var2_name]
             remove = (var1_name if (var1_score < var2_score) else var2_name)
             removed.append(remove)
             
-        print "Correlation between feature pairs: ", pairs
+        print "\nCorrelation between feature pairs: ", pairs
         print "Suggested features to remove due to high correlation: ", removed
-
-
-
+        
+        
+        
         ### Recursive feature selection
-        clf = RidgeClassifierCV() ## recursive features selection requires linear model
+        clf = LogisticRegression() ## recursive features selection requires linear model
         rfe = RFE(clf, n)
         rfe.fit(features_train, labels_train)
         recursive_selection = [featr for idx, featr in enumerate(predictors) if list(rfe.support_)[idx]]
-        print "\nBest features from recursive features selection: ", recursive_selection
+        
+        ## scores for aggregation
+        rec_rank = list(enumerate(recursive_selection[::-1], start = 1))
+        for e in rec_rank:
+            total_rank[e[1]] += e[0]
+        
+        print "\nBest features from recursive features selection (low to high): ", rec_rank
         
         
         
@@ -135,34 +175,42 @@ def select_best(data_dict, features_list, n = 10):
         from sklearn.tree import DecisionTreeClassifier
         clf = DecisionTreeClassifier()
         clf.fit(features_train, labels_train)
-        dt_imp = sorted(zip(clf.feature_importances_, predictors), key=lambda x: x[0], reverse=True)
-        dt = [pair[1] for pair in dt_imp[:n]]
+        dt_imp = zip(predictors, clf.feature_importances_)
+        dt_imp_dict = {feature: value for (feature, value) in dt_imp}
+            
         from sklearn.ensemble import RandomForestClassifier
         clf = RandomForestClassifier()
         clf.fit(features_train, labels_train)
-        rf_imp = sorted(zip(clf.feature_importances_, predictors), key=lambda x: x[0], reverse=True)
-        rf = [pair[1] for pair in rf_imp[:n]]
+        rf_imp = zip(predictors, clf.feature_importances_)
+        rf_imp_dict = {feature: value for (feature, value) in rf_imp}
+            
         from sklearn.ensemble import AdaBoostClassifier
         clf = AdaBoostClassifier()
         clf.fit(features_train, labels_train)
-        ab_imp = sorted(zip(clf.feature_importances_, predictors), key=lambda x: x[0], reverse=True)
-        ab = [pair[1] for pair in ab_imp[:n]]
-        print "\nBest features from Decision Tree: ", dt
-        print "Best features from Random Forest: ", rf
-        print "Best features from AdaBoost: ", ab
+        ab_imp = zip(predictors, clf.feature_importances_)
+        ab_imp_dict = {feature: value for (feature, value) in ab_imp}
+        
+        ginis = []
+        for ftr in predictors:
+            ginis.append([ftr, dt_imp_dict[ftr] + rf_imp_dict[ftr] + ab_imp_dict[ftr]])
+        ginis = sorted(ginis, key=lambda x: x[1], reverse = True)
+        gini_selection = [pair[0] for pair in ginis[:n]]
+        
+        ## scores for aggregation
+        gini_rank = list(enumerate(gini_selection[::-1], start = 1))
+        for e in gini_rank:
+            total_rank[e[1]] += e[0]
+
+        print "\nBest features from tree-based classifiers (low to high): ", gini_rank
         
         
         
-        ### getting best features from all methods
-        all_features = kbest_selection + recursive_selection + dt + rf + ab
-        unique_features_count = []
-        for feat in list(set(all_features)):
-            unique_features_count.append([feat, all_features.count(feat)])
-            
-        unique_features_count = sorted(unique_features_count, key=lambda x: x[1], reverse=True)
-        #max_num = max(unique_features_count, key=lambda x: x[1])[1] ## if we want to keep based upon some frequency number
-        keepers = [ft[0] for ft in unique_features_count[0:n]]
-        print "\nBest features from all selection methods: ", keepers
+        ### getting n best features from all methods
+        
+        total_rank = sorted(total_rank.items(), key = lambda x: x[1], reverse = True)
+        print "\nTotal rank of all features from all selection methods: ", total_rank
+        keepers = [ft[0] for ft in total_rank[:n]]
+        print "Best", n, "features from all selection methods: ", keepers
         
         
         return keepers
